1a006593f93ac2e30d31aad93a437faaae00cdbf
Koji Sato <utautaouka@gmail.com>
2025-02-24 14:16:11 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Item } from './pages/Item'
+import { Month } from './pages/Month'
+
+function App() {
+  return (
+    <>
+      <Item />
+      <Month />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Month.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Month = () => {
+  return (
+    <div>Month</div>
+  )
+}

fada13daffc01cb6b1e8122df7d48994e5a94a73
Koji Sato <utautaouka@gmail.com>
2025-04-20 15:25:58 +0900

    ログインフォームのテキストサイズを修正
    iOSの場合に入力するときに勝手にズームされてしまうため、font-sizeを16px以上に変更

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -75,7 +75,7 @@ export const Login = () => {
 							<input
 								type="email"
 								name="email"
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 								required
 								value={mail}
 								onChange={(e) => setEmail(e.target.value)}
@@ -94,7 +94,7 @@ export const Login = () => {
 							<input
 								type="password"
 								name="password"
-								className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 								required
 								value={password}
 								onChange={(e) => setPassword(e.target.value)}
